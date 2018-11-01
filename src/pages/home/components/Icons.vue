b3c78cb4e91bf4ac9ef77f81ece3f226357ff60b
leonaries <18382336395@163.com>
2018-11-01 18:09:22 +0800

    'recommend'

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -88,38 +88,40 @@
     }
 </script>
 
-<style scoped lang="stylus">
+<style lang="stylus"  scoped >
     @import "~styles/mixins.styl"
     @import "~styles/varibles.styl"
     .icons >>> .swiper-container
-        height 0
-        padding-bottom 50%
-    .icon
-        position relative
-        overflow hidden
-        float left
-        width 25%
-        padding-bottom 25%
-        .icon-img
-            position absolute
-            top 0
-            left 0
-            right 0
-            bottom 0.44rem
-            box-sizing border-box
-            padding .1rem
-            .icon-img-content
-                height:100%
-                display block
-                margin 0 auto
-        .icon-desc
-            position absolute
-            left 0
-            right 0
-            bottom 0
-            height .44rem
-            line-height .44rem
-            color $darkText-Color
-            text-align center
-            ellipsis ()
+        height: 0
+        padding-bottom: 50%
+    .icons
+        margin-top .2rem
+        .icon
+            position: relative
+            overflow: hidden
+            float: left
+            width: 25%
+            padding-bottom: 25%
+            .icon-img
+                position: absolute
+                top: 0
+                left: 0
+                right: 0
+                bottom: 0.44rem
+                box-sizing: border-box
+                padding: .1rem
+                .icon-img-content
+                    height:100%
+                    display: block
+                    margin: 0 auto
+            .icon-desc
+                position: absolute
+                left: 0
+                right: 0
+                bottom: 0
+                height: .44rem
+                line-height: .44rem
+                color: $darkText-Color
+                text-align: center
+                ellipsis ()
 </style>
---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,87 @@
+<template>
+    <div>
+        <div class="recommend-title">热销推荐</div>
+        <ul>
+            <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+                   <img class="item-img" :src="item.imgUrl">
+                   <div class="item-info">
+                       <p class="item-title">{{item.title}}</p>
+                       <p class="item-desc">{{item.desc}}</p>
+                       <button class="item-button">查看详情</button>
+                   </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HomeRecommend",
+        data () {
+            return {
+                recommendList :[
+                    {
+                        id:'0001',
+                        imgUrl:'http://img1.qunarzz.com/sight/p0/1807/ad/ad574bfe6c9e1ceda3.img.png_200x200_ea7a0734.png',
+                        title:'成都欢乐谷',
+                        desc:'超级旋转过山车刺激到让你怀疑人生超级旋转过山车刺激到让你怀疑人生'
+                    },
+                    {
+                        id:'0002',
+                        imgUrl:'http://img1.qunarzz.com/sight/p0/1807/ad/ad574bfe6c9e1ceda3.img.png_200x200_ea7a0734.png',
+                        title:'成都欢乐谷',
+                        desc:'超级旋转过山车刺激到让你怀疑人生超级旋转过山车刺激到让你怀疑人生'
+                    },
+                    {
+                        id:'0003',
+                        imgUrl:'http://img1.qunarzz.com/sight/p0/1807/ad/ad574bfe6c9e1ceda3.img.png_200x200_ea7a0734.png',
+                        title:'成都欢乐谷',
+                        desc:'超级旋转过山车刺激到让你怀疑人生超级旋转过山车刺激到让你怀疑人生'
+                    },
+                    {
+                        id:'0004',
+                        imgUrl:'http://img1.qunarzz.com/sight/p0/1807/ad/ad574bfe6c9e1ceda3.img.png_200x200_ea7a0734.png',
+                        title:'成都欢乐谷',
+                        desc:'超级旋转过山车刺激到让你怀疑人生超级旋转过山车刺激到让你怀疑人生'
+                    },
+                ]
+            }
+        }
+    }
+</script>
+
+<style scoped lang="stylus">
+    @import "~styles/mixins.styl"
+    .recommend-title
+        margin-top .2rem
+        line-height .8rem
+        background  #eee
+        text-indent .2rem
+    .item
+        overflow hidden
+        display flex
+        height 1.9rem
+        .item-img
+            width:1.7rem
+            height 1.7rem
+            padding .1rem
+        .item-info
+            flex 1
+            min-width 0
+            padding .1rem
+            .item-title
+                line-height .54rem
+                font-size .32rem
+                ellipsis ()
+            .item-desc
+                line-height .4rem
+                color #ccc
+                ellipsis ()
+            .item-button
+                background #ff9300
+                line-height .44rem
+                margin-top: .16rem
+                padding 0  .2rem
+                border-radius 0.06rem
+                color #fff
+</style>
