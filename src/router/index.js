17c671ae4ff2ecf8ea624cb58049fbb10939cd69
leonaries <18382336395@163.com>
2018-11-06 15:16:36 +0800

    detail-ajax

--- src/router/index.js
+++ src/router/index.js
@@ -22,5 +22,9 @@ export default new Router({
           name: 'Detail',
           component: Detail
       }
-  ]
+  ],
+    //当页面被拖动到下面时 平时切换路由视图 还是会显示在西面，加上这个函数处理右后切换路由会使得页面恢复到顶部
+    scrollBehavior (to,from,savedPosition) {
+      return { x:0,y:0}
+    }
 })
